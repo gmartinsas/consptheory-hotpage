81555042a8591c0b2a182861416ff8f04f75606b
Ariel Martins <gabrielmartinsas@outlook.com>
2022-11-26 20:46:33 -0300

    Formatação index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IllustrationComponent } from './components/illustration/illustration.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { ConfirmaPageComponent } from './components/confirma-page/confirma-page.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { FormsComponent } from './components/forms/forms.component';
     NavbarComponent,
     IllustrationComponent,
     FormsComponent,
+    ConfirmaPageComponent,
   ],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
